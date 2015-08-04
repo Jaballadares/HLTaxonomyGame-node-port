d4ba3608eacc22e2e540a3a7b2b894173ad3fd5c
jaballadares <jballadares28@gmail.com>
2015-08-04 00:18:57 -0700

    trying to get js to work

--- js/board.js
+++ js/board.js
@@ -1,17 +1,57 @@
 var concept = document.getElementById('concept');
 var test = document.getElementById('test');
 
+// To create dom element with this function
+  // example div with class of mine you would
+// elt("div", {class: "mine"});
 
+function elt(name, attributes) {
+  var node = document.createElement(name);
+  if(attributes) {
+    for (var attr in attributes)
+      if (attributes.hasOwnProperty(attr))
+        node.setAttribute(attr, attributes[attr]);
+  }
+  for (var i = 2; i < arguments.length; i++) {
+    var child = arguments[i];
+    if (typeof child == "string")
+      child = document.createTextNode(child);
+    node.appendChild(child);
+  }
+  return node;
+}
+
+function load() {
+  $.ajax({
+    dataType: "json",
+    url: "./hltaxonomy.json",
+    success: function(data){
+      for(var i=0;i < data.length; i++){
+        var myData = data[i];
+        console.log(myData);
+console.log(data[i]["Medical Concept"]);
+
+}
+    },
+    error: function(e){
+      console.log(e);
+    }
+  });
+}
+load();
+
+
+// Adding click event listener for concept being chosen in the sidebar list
 concept.addEventListener("click", function() {
-  this.style.border = "1px solid white";
-  this.style.color = "purple";
-  this.style.background = "rgba(255,255,255,0.4)";
+  this.setAttribute("class", "concept_plucked");
 
-// takes the concepts innerText
+  // takes the concepts innerText
   var innerText = this.innerText;
 
-// adds innerText from concept into placeholder
+  // adds innerText from concept into placeholder
   test.onclick = function() {
     this.innerText = innerText;
-  }
+    this.style.background = "purple";
+    this.style.color = "white";
+  };
 });
